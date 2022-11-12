d4d7098356b980d5da339bfe6c31149519929a5e
Jonathan <machadofjonathan@gmail.com>
2022-11-12 18:59:55 +0000

    working implementation

--- src/components/VSelect.vue
+++ src/components/VSelect.vue
@@ -1,63 +1,54 @@
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, computed, watch } from 'vue'
 
 const props = defineProps({
     option: Object,
     state: Object,
     trackBy: { type: String, default: 'id' }
 })
+let selectedOption = ref({})
 
-const localState = ref({
-    "category": "created_at",
-    "operator": "is_equals",
-    "value": "1990-01-01",
-})
+// watch state[props.trackBy]
+watch(() => props.state[props.trackBy], (value) => {
+    selectedOption.value = props.option.dropdownItems?.find(item => item.id === value)
+    console.log(selectedOption.value);
+}, { immediate: true })
 
-const selectedOption = computed(() => {
-    /**
-     * This relates to the selected `option` in the Select component.
-     * Find if any of the `options` is selected according to the `state`
-     * */
 
-    // During recursion:
-    // On the first iteration state.field should be used to find the selected option
-    // On the second iteration: state.operator.
-    // It seems that on each iteration the `option` needs to be aware of which `field` to use
-    // Let's solve that by requiring every `option` object to have a `trackBy` propertyd
-    return props.option.dropdownItems?.find(option => option.id === props.state[props.trackBy])
-})
+
+const setChildren = () => {
+    selectedOption.value = {
+        children: [
+            { type: "text", label: "Date", id: 4, trackBy: "value" },
+            { type: "text", label: "Date", id: 4, trackBy: "value" }]
+    }
+}
 
 </script>
 
 <template>
-    <!-- -->
-    <select v-if="option.type == 'dropdown'" v-model="state[option.trackBy]">
+    <select v-if="option.type == 'dropdown'" v-model="state[props.trackBy]">
         <option v-for="item in option.dropdownItems" :key="item.id" :value="item.id">{{ item.label }}</option>
     </select>
 
-    <input v-if="option.type == 'date'" v-model="state[option.trackBy]" type="date" :id="option.id">
-
-    <div v-if="option.type == 'user-answers'">
-        I need to allow this component to set `selectedOption`
-
-        <!-- Write operation failed: computed value is readonly -->
-        <button @click="selectedOption = {
-            children: [{type: ' date', label: 'Date', id: 4, trackBy: 'value'}],
-        }">Set</button>
-    </div>
-
+    <!-- Recursion -->
     <div v-if="selectedOption?.children">
         <div v-for="child in selectedOption.children" :key="child.id">
-            <VSelect :option="child" :state="state" :trackBy="child.trackBy" />
-
-            <!-- Children, based on the selected item -->
-            <!-- <div v-if="state[child.trackBy].children">
-            <div v-for="(child2, idx) in state[child.trackBy].children" :key="idx">
-                <input v-if="child2.type == 'date'" v-model="state[child2.trackBy]" type="date" :id="child2.id">
-            </div>
-        </div> -->
-
+            <!-- PS: `key` is important here otherwise Vue will try to reuse the component in the DOM, which will have the wrong state  -->
+            <VSelect :option="child" :state="state" :trackBy="child.trackBy" :key="state[props.trackBy]" />
         </div>
+    </div>
+
+    <!-- Leaf nodes -->
+    <input v-if="option.type == 'date'" v-model="state[option.trackBy]" type="date" :id="option.id">
+
+    <input v-if="option.type == 'text'" v-model="state[option.trackBy]" type="text" :id="option.id">
+
+    <div v-if="option.type == 'user-answers'">
+        <div>
+            <button @click="setChildren">Dynamically set children</button>
+        </div>
+
     </div>
 
 </template>
